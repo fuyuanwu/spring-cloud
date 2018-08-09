02dae4b7802eed11d04e93317068f47c5b85b5e2
fyw_wu <fyw_wu@sina.com>
2018-08-09 15:35:58 +0800

    初始化项目

---
+++ config-server/src/main/java/com/pobo/spring/security/demo/configuration/ConfigConfiguration.java
@@ -0,0 +1,12 @@
+package com.pobo.spring.security.demo.configuration;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fuyuanwu
+ */
+@Configuration
+@EnableConfigServer
+public class ConfigConfiguration {
+}
